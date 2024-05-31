12e2a86032c49676e0060db5e9ec931393c0dd7f
ramironunes <ramronunes@gmail.com>
2024-05-30 23:16:17 -0300

    Correction to the create_project script

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -2,7 +2,7 @@
 # @Author: Ramiro Luiz Nunes
 # @Date:   2024-05-30 22:22:16
 # @Last Modified by:   Ramiro Luiz Nunes
-# @Last Modified time: 2024-05-30 22:58:54
+# @Last Modified time: 2024-05-30 23:06:23
 
 # Project name
 PROJECT_NAME="react-frontend-core"
@@ -14,11 +14,11 @@ cd $PROJECT_NAME
 
 # Install additional libraries
 echo -e "${BLUE}Installing additional libraries (axios, react-router-dom)...${NC}"
-npm install axios react-router-dom
+npm install axios react-router-dom --legacy-peer-deps
 
 # Install React Admin and Material-UI
 echo -e "${BLUE}Installing React Admin and Material-UI...${NC}"
-npm install ra-core ra-ui-materialui @material-ui/core @material-ui/icons
+npm install ra-core ra-ui-materialui @material-ui/core @material-ui/icons --legacy-peer-deps
 
 # Create logs directory
 mkdir -p logs
@@ -29,3 +29,7 @@ jq '.scripts.start = "react-scripts start 2>&1 | tee logs/start.log" |
     .scripts.build = "react-scripts build 2>&1 | tee logs/build.log" |
     .scripts.test = "react-scripts test 2>&1 | tee logs/test.log" |
     .scripts.eject = "react-scripts eject 2>&1 | tee logs/eject.log"' package.json > package.json.tmp && mv package.json.tmp package.json
+
+# Ensure all dependencies are installed
+echo -e "${BLUE}Ensuring all dependencies are installed...${NC}"
+npm install --legacy-peer-deps
