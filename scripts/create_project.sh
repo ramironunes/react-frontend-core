a7eac63c3426e4d6e3ad492a326bec7d841ce933
ramironunes <ramronunes@gmail.com>
2024-05-30 22:59:58 -0300

    feat: add scripts to automate React project setup with log redirection
    - Configured npm scripts to redirect logs to the logs directory

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -2,7 +2,7 @@
 # @Author: Ramiro Luiz Nunes
 # @Date:   2024-05-30 22:22:16
 # @Last Modified by:   Ramiro Luiz Nunes
-# @Last Modified time: 2024-05-30 22:36:25
+# @Last Modified time: 2024-05-30 22:58:54
 
 # Project name
 PROJECT_NAME="react-frontend-core"
@@ -19,3 +19,13 @@ npm install axios react-router-dom
 # Install React Admin and Material-UI
 echo -e "${BLUE}Installing React Admin and Material-UI...${NC}"
 npm install ra-core ra-ui-materialui @material-ui/core @material-ui/icons
+
+# Create logs directory
+mkdir -p logs
+
+# Update package.json to include log redirection
+echo -e "${BLUE}Configuring npm scripts to redirect logs...${NC}"
+jq '.scripts.start = "react-scripts start 2>&1 | tee logs/start.log" |
+    .scripts.build = "react-scripts build 2>&1 | tee logs/build.log" |
+    .scripts.test = "react-scripts test 2>&1 | tee logs/test.log" |
+    .scripts.eject = "react-scripts eject 2>&1 | tee logs/eject.log"' package.json > package.json.tmp && mv package.json.tmp package.json
