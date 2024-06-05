a2662cf21ae3e8d9146b52c8bde1fda27f52a494
ramironunes <ramronunes@gmail.com>
2024-06-04 23:10:29 -0300

    Add template choice to install script and update manage_project script

--- scripts/install_project.sh
+++ scripts/install_project.sh
@@ -2,7 +2,7 @@
 # @Author: Ramiro Luiz Nunes
 # @Date:   2024-05-30 22:02:41
 # @Last Modified by:   Ramiro Luiz Nunes
-# @Last Modified time: 2024-06-02 20:24:32
+# @Last Modified time: 2024-06-04 23:09:15
 
 # Import the functions from utils.sh
 source scripts/utils.sh
@@ -17,6 +17,25 @@ fi
 echo -e "${GREEN}================================================================================${NC}"
 echo -e "${CYAN}                         Installing React Frontend Core                         ${NC}"
 echo -e "${GREEN}================================================================================${NC}"
+
+# Prompt user to select the template type
+echo "Select the project template type:"
+echo "1. TypeScript"
+echo "2. JavaScript"
+read -p "Enter your choice (1 or 2): " template_choice
+
+case $template_choice in
+    1)
+        template="--template typescript"
+        ;;
+    2)
+        template=""
+        ;;
+    *)
+        echo -e "${RED}Invalid choice. Exiting...${NC}"
+        exit 1
+        ;;
+esac
 
 # Redirect output to a log file
 LOG_FILE="logs/install_project.log"
@@ -34,14 +53,11 @@ print_separator
 
 # Create project and install libraries
 echo -e "${YELLOW}Creating project and installing libraries...${NC}"
-if ! source scripts/create_project.sh; then
+if ! npx create-react-app . $template; then
     echo -e "${RED}Project creation failed. See ${LOG_FILE} for details.${NC}"
     exit 1
 fi
-print_separator
 
-# Setup configuration files
-echo -e "${YELLOW}Setting up configuration files...${NC}"
 if ! source scripts/setup_utils.sh; then
     echo -e "${RED}Setup configuration failed. See ${LOG_FILE} for details.${NC}"
     exit 1
